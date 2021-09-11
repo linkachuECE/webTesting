d5ebe689cfe342c2f6f2ef696a907534ecf4fe2d
Ethan <ethangbraun@gmail.com>
2021-09-10 23:42:52 -0500

    still messing around. This isn't the worst, I suppose

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
-var x;
-if(x === undefined)
-    console.log("is this working");
-else if(x === 0)
-    console.log("It's zero")
-else
-    console.log("it would appear so");
+var x = 9;
+
+
+try{
+    if(x ==5){
+        console.log("you did it");
+    } else {
+        throw new Error("not 5");
+    }
+} catch (e){
+    console.error(e.name);
+    console.error(e.message);
+} finally {
+    console.log("made it");
+}
